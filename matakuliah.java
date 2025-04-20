b226c74d2901eb5fabc0d6c5847fa10cf4b2a98d
Nizam El <nizamel1402@gmail.com>
2025-04-20 22:29:28 +0700

    Add files via upload

---
+++ matakuliah.java
@@ -0,0 +1,14 @@
+public class matakuliah {
+    public String kodeMK;
+    public String namaMK;
+    public int sks;
+
+    public matakuliah (String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+    public void tampilMataKuliah() {
+        System.out.println("Kode MK: " + kodeMK + " | Nama: " + namaMK +" | SKS: " + sks);
+    }
+}
